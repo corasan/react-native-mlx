c7e3fb9be7782c1da25852d74fd51e3bebc48662
Henry Paulino <me@henrypl.com>
2025-02-12 15:26:03 +0000

    create xcframework bundle

---
+++ package/ios/RNMLX.xcframework/ios-arm64/RNMLX.framework/Headers/RNMLX.h
@@ -0,0 +1,18 @@
+//
+//  RNMLX.h
+//  RNMLX
+//
+//  Created by Henry on 2/12/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNMLX.
+FOUNDATION_EXPORT double RNMLXVersionNumber;
+
+//! Project version string for RNMLX.
+FOUNDATION_EXPORT const unsigned char RNMLXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNMLX/PublicHeader.h>
+
+
