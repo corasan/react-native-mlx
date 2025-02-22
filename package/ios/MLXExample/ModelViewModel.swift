5d4ff9dbf21b490e22663c7bb1b1efc7bb8790ec
Henry Paulino <me@henrypl.com>
2025-02-22 16:04:28 -0500

    example app

---
+++ package/ios/MLXExample/ModelViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  ModelViewModel.swift
+//  MLXExample
+//
+//  Created by Henry on 2/22/25.
+//
+
+import Foundation
+import ReactNativeMLX
+
+@MainActor
+class ModelViewModel: ObservableObject, RNMLXDelegate {
+    @Published var status: String = ""
+    @Published var error: String = ""
+    @Published var isLoading = false
+    @Published var downloadProgress: Double = 0
+    @Published var currentFile: String = ""
+    @Published var output: String = ""
+    @Published var tokensPerSecond: Double = 0
+    
+    private let mlx = RNMLX()
+    
+    init() {
+        mlx.delegate = self
+    }
+    
+    func modelLoadingProgress(progress: Double, file: String) {
+        downloadProgress = progress
+        currentFile = file
+    }
+    
+    func modelGenerationProgress(text: String, tokensPerSecond: Double) {
+        output = text
+        self.tokensPerSecond = tokensPerSecond
+    }
+    
+    func modelStateChanged(state: ModelState) {
+        isLoading = !state.isLoaded
+        if let error = state.error {
+            self.error = error
+        }
+    }
+    
+    func modelError(_ error: Error) {
+        self.error = error.localizedDescription
+    }
+    
+    func loadLlama() async {
+        await loadModel(
+            name: "Llama 3.2 1B Instruct 4bit",
+            modelId: "llama-3.1b-instruct-4bit",
+            repoId: "mlx-community/Llama-3.2-1B-Instruct-4bit"
+        )
+    }
+    
+    private func loadModel(name: String, modelId: String, repoId: String) async {
+        guard !isLoading else { return }
+        
+        isLoading = true
+        error = ""
+        downloadProgress = 0
+        
+        do {
+            try await mlx.load(modelId: modelId)
+//            try await mlx.generate(prompt: "Say hello!")
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+}
