a7d1eda243991d9097109c26b2a3997874505c0e
Henry Paulino <me@henrypl.com>
2025-02-22 16:04:40 -0500

    download model

---
+++ package/ios/Sources/LLMEvaluator.swift
@@ -0,0 +1,142 @@
+//
+//  LLMEvaluator.swift
+//  ReactNativeMLX
+//
+//  Created by Henry on 2/22/25.
+//
+
+import Foundation
+internal import MLX
+internal import MLXLLM
+internal import MLXLMCommon
+internal import MLXRandom
+
+@MainActor
+class LLMEvaluator {
+    var progressHandler: ((Double, String) -> Void)?
+    var generationHandler: ((String, Double) -> Void)?
+    var running = false
+
+    var includeWeatherTool = false
+
+    var output = ""
+    var modelInfo = ""
+    var stat = ""
+
+    /// This controls which model loads. `qwen2_5_1_5b` is one of the smaller ones, so this will fit on
+    /// more devices.
+    let modelConfiguration = ModelRegistry.llama3_2_1B_4bit
+
+    /// parameters controlling the output
+    let generateParameters = GenerateParameters(temperature: 0.6)
+    let maxTokens = 240
+
+    /// update the display every N tokens -- 4 looks like it updates continuously
+    /// and is low overhead.  observed ~15% reduction in tokens/s when updating
+    /// on every token
+    let displayEveryNTokens = 4
+
+    enum LoadState {
+        case idle
+        case loaded(ModelContainer)
+    }
+
+    var loadState = LoadState.idle
+
+    let currentWeatherToolSpec: [String: any Sendable] =
+        [
+            "type": "function",
+            "function": [
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": [
+                    "type": "object",
+                    "properties": [
+                        "location": [
+                            "type": "string",
+                            "description": "The city and state, e.g. San Francisco, CA",
+                        ] as [String: String],
+                        "unit": [
+                            "type": "string",
+                            "enum": ["celsius", "fahrenheit"],
+                        ] as [String: any Sendable],
+                    ] as [String: [String: any Sendable]],
+                    "required": ["location"],
+                ] as [String: any Sendable],
+            ] as [String: any Sendable],
+        ] as [String: any Sendable]
+
+    /// load and return the model -- can be called multiple times, subsequent calls will
+    /// just return the loaded model
+    func load() async throws -> ModelContainer {
+        switch loadState {
+        case .idle:
+            MLX.GPU.set(cacheLimit: 20 * 1024 * 1024)
+
+            let modelContainer = try await LLMModelFactory.shared.loadContainer(
+                configuration: modelConfiguration
+            ) { [modelConfiguration] progress in
+                Task { @MainActor in
+                    let p = progress.fractionCompleted * 100
+                    self.progressHandler?(p, modelConfiguration.name)
+                }
+            }
+            
+            loadState = .loaded(modelContainer)
+            return modelContainer
+
+        case .loaded(let modelContainer):
+            return modelContainer
+        }
+    }
+
+    func generate(prompt: String) async {
+        guard !running else { return }
+
+        running = true
+        self.output = ""
+
+        do {
+            let modelContainer = try await load()
+
+            // each time you generate you will get something new
+            MLXRandom.seed(UInt64(Date.timeIntervalSinceReferenceDate * 1000))
+
+            let result = try await modelContainer.perform { context in
+                let input = try await context.processor.prepare(
+                    input: .init(
+                        messages: [
+                            ["role": "system", "content": "You are a helpful assistant."],
+                            ["role": "user", "content": prompt],
+                        ], tools: includeWeatherTool ? [currentWeatherToolSpec] : nil))
+                return try MLXLMCommon.generate(
+                    input: input, parameters: generateParameters, context: context
+                ) { tokens in
+                    // Show the text in the view as it generates
+                    if tokens.count % displayEveryNTokens == 0 {
+                        let text = context.tokenizer.decode(tokens: tokens)
+                        Task { @MainActor in
+                            self.output = text
+                        }
+                    }
+                    if tokens.count >= maxTokens {
+                        return .stop
+                    } else {
+                        return .more
+                    }
+                }
+            }
+
+            // update the text if needed, e.g. we haven't displayed because of displayEveryNTokens
+            if result.output != self.output {
+                self.output = result.output
+            }
+            self.stat = " Tokens/second: \(String(format: "%.3f", result.tokensPerSecond))"
+
+        } catch {
+            output = "Failed: \(error)"
+        }
+
+        running = false
+    }
+}
