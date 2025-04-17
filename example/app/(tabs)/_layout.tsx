eeac8deee83d212d7169761c720738bccaa59862
Henry Paulino <me@henrypl.com>
2025-04-16 20:42:45 -0400

    Promise based token generation (#3)
    * Update release-it and related plugins to latest versions
    
    * Add streaming generation API
    
    * Add advanced and context chat screens
    
    * Update _layout.tsx
    
    * Fix keyExtractor and integrate MLXProvider
    
    * Remove unused Tab Two screen and fix linting issues

--- example/app/(tabs)/_layout.tsx
+++ example/app/(tabs)/_layout.tsx
@@ -1,28 +1,48 @@
 import { useClientOnlyValue } from '@/components/useClientOnlyValue'
 import { useColorScheme } from '@/components/useColorScheme'
 import Colors from '@/constants/Colors'
-import { Stack } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
 import type React from 'react'
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name']
+  color: string
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />
+}
 
 export default function TabLayout() {
   const colorScheme = useColorScheme()
 
   return (
-    <Stack
+    <Tabs
       screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: useClientOnlyValue(false, true),
-        contentStyle: {
-          backgroundColor: Colors[colorScheme ?? 'light'].background,
-          paddingHorizontal: 16,
-        },
       }}
     >
-      <Stack.Screen
+      <Tabs.Screen
         name="index"
         options={{
-          title: 'Chat',
+          title: 'Classic',
+          tabBarIcon: ({ color }) => <TabBarIcon name="comment" color={color} />,
         }}
       />
-    </Stack>
+      <Tabs.Screen
+        name="advanced"
+        options={{
+          title: 'Streaming',
+          tabBarIcon: ({ color }) => <TabBarIcon name="rocket" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="context"
+        options={{
+          title: 'Context',
+          tabBarIcon: ({ color }) => <TabBarIcon name="cloud" color={color} />,
+        }}
+      />
+    </Tabs>
   )
 }
